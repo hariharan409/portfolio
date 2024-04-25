6c86d219421f49ae9dbc1aaa38cd0461fb5432a3
hariharan <hharan987@gmail.com>
2024-04-25 17:57:14 +0800

    Project added to git repo commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HariPortFolio from './pages/portfolio/Index';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-root-component">
+        <Routes>
+          <Route path='/' Component={HariPortFolio}/>
+        </Routes>
     </div>
   );
 }
