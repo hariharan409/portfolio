bf97b7268dd979f1ee380e72f545c15bcd9a42d0
hariharan <hharan987@gmail.com>
2024-04-25 19:57:21 +0800

    basename update commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/portfolio'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
