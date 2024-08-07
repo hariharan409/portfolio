983f8b15b86960d2d5bec8160a22094749ccbbae
hariharan409 <hharan987@gmail.com>
2024-08-07 14:34:44 +0800

    ecosystem ui fix commit

--- src/pages/portfolio/Introduction.js
+++ src/pages/portfolio/Introduction.js
@@ -21,11 +21,11 @@ const Introduction = () => {
             <div className="portfolio-section-two-ecosystem-element mt-3">
                 {(developerEcosystemList instanceof Array && developerEcosystemList.length > 0) &&
                     developerEcosystemList.map((ecosystem) => {
-                return  <div key={ecosystem.id} className="portfolio-section-two-ecosystem-element-item" style={{paddingTop: "20px",paddingBottom: "70px",position: "relative"}}>
+                return  <div key={ecosystem.id} className="portfolio-section-two-ecosystem-element-item" style={{paddingTop: "20px",paddingBottom: "70px"}}>
                             <h5 className="text-uppercase">{ecosystem.ecosystemName}</h5>
                             <img src={ecosystem.ecosystemImage} style={{width: "80px",borderRadius: "50%",marginBlock: "10px"}} />
-                            <span style={{fontSize: "small",display: "block"}}>{ecosystem.ecosystemDescription}</span>
-                            <a href={ecosystem.link} target="_blank" style={{position: "absolute",bottom: 10}}>
+                            <span style={{fontSize: "small",display: "block",minHeight: "80px"}}>{ecosystem.ecosystemDescription}</span>
+                            <a href={ecosystem.link} target="_blank">
                                 <button type="button" className="btn btn-primary p-2 mt-3 text-uppercase" style={{width: "130px",fontSize: "12px"}}>{ecosystem.ecosystemName}</button>
                             </a>
                         </div>
