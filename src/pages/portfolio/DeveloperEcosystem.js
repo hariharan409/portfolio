032bfe8f91bb439f7a5f2ef89f996d9a766decd5
hariharan409 <hharan987@gmail.com>
2024-08-07 18:54:21 +0800

    ecosystem css name updated

--- src/pages/portfolio/DeveloperEcosystem.js
+++ src/pages/portfolio/DeveloperEcosystem.js
@@ -4,10 +4,10 @@ import { developerEcosystemList } from "./static-files/portfolioList";
 const DeveloperEcosystem = () => {
 
     return(
-        <div className="portfolio-section-two-ecosystem-element mt-3">
+        <div className="ecosystem-element mt-3">
                 {(developerEcosystemList instanceof Array && developerEcosystemList.length > 0) &&
                     developerEcosystemList.map((ecosystem) => {
-                return  <div key={ecosystem.id} className="portfolio-section-two-ecosystem-element-item" style={{paddingTop: "20px",paddingBottom: "70px"}}>
+                return  <div key={ecosystem.id} className="ecosystem-element-item" style={{paddingTop: "20px",paddingBottom: "70px"}}>
                             <h5 className="text-uppercase">{ecosystem.ecosystemName}</h5>
                             <img src={ecosystem.ecosystemImage} style={{width: "80px",borderRadius: "50%",marginBlock: "10px"}} />
                             <span style={{fontSize: "small",display: "block",minHeight: "80px"}}>{ecosystem.ecosystemDescription}</span>
